ddaec8aba74996c758287d82e8b7930214bcc285
Md Aiman Sharif <aiman.sharif@youi.tv>
2020-10-23 01:57:54 -0400

    Final Commit YelpCamp completed

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 
 var campgroundSchema = new mongoose.Schema({
     name: String,
+    price: Number,
     image: String,
     description: String,
     author: {
